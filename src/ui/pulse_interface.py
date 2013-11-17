0fa3b05983ba1b62b02b7213e272d5499681620f
bsuire <bensuire@hotmail.com>
2013-11-17 15:29:40 -0500

    pulse interface finished. 1. Requests data from pulse API 2. Creates a dictionary with date, weather and power

--- src/ui/pulse_interface.py
+++ src/ui/pulse_interface.py
@@ -4,65 +4,94 @@ Created on Nov 16, 2013
 @author: USER
 '''
 
-
 import urllib2, datetime, ast
 from datetime import timedelta
 
 # Constants
-power = 50578
-radiation = 66094
-humidity = 66095
-temperature = 66077
-wind = 66096
+power_code = 50578
+radiation_code = 66094
+humidity_code = 66095
+temperature_code = 66077
+wind_code = 66096
 
 sample_input = str({"id":66077,"label":"Montreal Temperature - CWTA","unit":"AC","quantity":"temperature","resource":"outside","start":"2013-11-15T23:00:00-05:00","end":"2013-11-17T00:00:00-05:00","average":8.243999999999998,"data":[["2013-11-15T23:00:00-05:00",7.7],["2013-11-16T00:00:00-05:00",7.7],["2013-11-16T01:00:00-05:00",7],["2013-11-16T02:00:00-05:00",6],["2013-11-16T03:00:00-05:00",7],["2013-11-16T04:00:00-05:00",5],["2013-11-16T05:00:00-05:00",5],["2013-11-16T06:00:00-05:00",5],["2013-11-16T07:00:00-05:00",4],["2013-11-16T08:00:00-05:00",4],["2013-11-16T09:00:00-05:00",7],["2013-11-16T10:00:00-05:00",9.8],["2013-11-16T11:00:00-05:00",11],["2013-11-16T12:00:00-05:00",12],["2013-11-16T13:00:00-05:00",12],["2013-11-16T14:00:00-05:00",12],["2013-11-16T15:00:00-05:00",12],["2013-11-16T16:00:00-05:00",11.7],["2013-11-16T17:00:00-05:00",11.4],["2013-11-16T18:00:00-05:00",10.8],["2013-11-16T19:00:00-05:00",10.2],["2013-11-16T20:00:00-05:00",7.7],["2013-11-16T21:00:00-05:00",7.2],["2013-11-16T22:00:00-05:00",6.7],["2013-11-16T23:00:00-05:00",6.2]]})
-
 
 def get_pulse_json(code,date):
   data = urllib2.urlopen("https://api.pulseenergy.com/pulse/1/points/"+str(code)+"/data.json?key=A366D4672C54464D423656BD2D3DBAA3&interval=day&start="+date).read() 
   return data
 
-
 def json_to_list(json):   
     list_out = []
     for x in json['data']:
-      y = x.pop()
-      list_out.append(y)
+      #y = x.pop()
+      #list_out.append(y)
+      list_out.append(x)
     return list_out
 
-def main():
+def interleave_weather(dates,weather_raw):
+  weather_formatted = []
+  for x in dates:
+    date_found = 0
+    for y in weather_raw:
+      if x == y[0]:
+        weather_formatted.append(y[1])
+        date_found = 1
+      #  break
+    if date_found == 0:
+      weather_formatted.append("")
+  
+  return weather_formatted
 
+def main():
   now = datetime.datetime.now()
   query_date = now - timedelta(hours=8)
-  query_date = query_date.strftime("%Y-%m-%dT%H:00-05:00")
-  print query_date
+  query_date = query_date.strftime("%Y-%m-%dT%H:%M-05:00")
+  now = now.strftime("%Y-%m-%dT%H:%M")
+  print now
 
-  power_data = get_pulse_json(power,query_date)
-  power_data = power_data.replace("null","-1")  
-  power_dict = ast.literal_eval(power_data)
-  power_list = json_to_list(power_dict)
+  power_raw = get_pulse_json(power_code,query_date)
+  power_raw = power_raw.replace("null","-1")  
+  power_raw = ast.literal_eval(power_raw)
+  power_raw = json_to_list(power_raw)
 
+  for x in power_raw:
+    print x
+
+  radiation_raw = get_pulse_json(radiation_code,query_date)
+  radiation_raw = ast.literal_eval(radiation_raw)
+  radiation_raw = json_to_list(radiation_raw)
   
-  for i in range (0, power_list.count(-1)):
-    power_list.remove(-1)
-
-  radiation_data = get_pulse_json(radiation,query_date)
-  radiation_dict = ast.literal_eval(radiation_data)
-  radiation_list = json_to_list(radiation_dict)
-
+  humidity_raw = get_pulse_json(humidity_code,query_date)
+  humidity_raw = ast.literal_eval(humidity_raw)
+  humidity_raw = json_to_list(humidity_raw)
   
-  humidity_data = get_pulse_json(humidity,query_date)
-  humidity_dict = ast.literal_eval(humidity_data)
-  humidity_list = json_to_list(humidity_dict)
-
-
-  temperature_data = get_pulse_json(temperature,query_date)
-  temperature_dict = ast.literal_eval(temperature_data)
-  temperature_list = json_to_list(temperature_dict)
+  temperature_raw = get_pulse_json(temperature_code,query_date)
+  temperature_raw = ast.literal_eval(temperature_raw)
+  temperature_raw = json_to_list(temperature_raw)
   
-  wind_data = get_pulse_json(wind,query_date)
-  wind_dict = ast.literal_eval(wind_data)
-  wind_list = json_to_list(wind_dict)
+  wind_raw = get_pulse_json(wind_code,query_date)
+  wind_raw = ast.literal_eval(wind_raw)
+  wind_raw = json_to_list(wind_raw)
+  
+  # CSV specs: 1st line should be dummy 
+  # DATE - RADIATION - HUMIDITY - TEMPERATURE - WIND - POWER
+  
+  # DATE/POWER
+  dates = []
+  power_stripped = []
+  for x in power_raw:
+    dates.append(x[0])
+    if x[1] != -1:
+      power_stripped.append(x[1])
+    else:
+      power_stripped.append('')
+  
+  radiation_interleaved = interleave_weather(dates,radiation_raw)
+  humidity_interleaved = interleave_weather(dates,humidity_raw)
+  temperature_interleaved = interleave_weather(dates,temperature_raw)
+  wind_interleaved = interleave_weather(dates,wind_raw)
+  
+  d_pulse = dict(date=dates, radiation=radiation_interleaved,humidity = humidity_interleaved,temperature=temperature_interleaved, wind=wind_interleaved, demand=power_stripped)
 
 
   
